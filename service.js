f6db91c04e5997404e955bbb23f99a15e1e147ae
stagiaire <stagiaire@cpg-453068.cepegra2008.lan>
2019-11-12 11:27:55 +0100

    espace suppression

--- service.js
+++ service.js
@@ -1,12 +1,12 @@
 const version="1.0.0"
 
 self.addEventListener("install", event => {
-    console.log("INSTALL service worker version " + version)
+    console.log("INSTALL service worker version" + version)
     return self.skipWaiting()
 })
 
 self.addEventListener("activate", event => {
-    console.log("ACTIVATE service worker version " + version)
+    console.log("ACTIVATE service worker version" + version)
     return self.skipWaiting()
     //return self.skipWaiting()
 })
