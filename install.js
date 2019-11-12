ad55d952cca035c0a96b1324d89a74f7402f2a52
stagiaire <stagiaire@cpg-453068.cepegra2008.lan>
2019-11-12 14:09:58 +0100

    test

--- install.js
+++ install.js
@@ -13,7 +13,6 @@
     });
 
     installBtn.addEventListener('click', (e) => {
-        e.preventDefault();
         installBtn.style.display = 'none';
         // Affiche la demande d’installation 
         deferredPrompt.prompt();
