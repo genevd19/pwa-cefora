9d2ac6ba12d1526ff58435003905387df1de8e98
stagiaire <stagiaire@cpg-453068.cepegra2008.lan>
2019-11-12 11:52:25 +0100

    cache

--- service.js
+++ service.js
@@ -13,3 +13,23 @@ self.addEventListener("activate", event => {
 self.addEventListener('fetch', function () {
 //met en ecoute le services
 })
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+    console.log('ok, workbox chargé');
+    workbox.precaching.precacheAndRoute([
+        {"url" : "index.html"
+        },
+        {"url" : "style.css"
+        },
+        {"url" : "manifest.json"
+        },
+        {"url" : "favicon.ico"
+        },
+        {"url" : "icon-96-96.png"
+        }
+    ])
+} else {
+    console.log('OOh, workbox non chargé');
+    
+}
