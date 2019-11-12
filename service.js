812d423d4d6867e5c2b5d9fef289ec19269b91fb
stagiaire <stagiaire@cpg-453068.cepegra2008.lan>
2019-11-12 11:18:50 +0100

    self add event

--- service.js
+++ service.js
@@ -9,3 +9,6 @@ self.addEventListener("activate", event =>{
     console.log("ACTIVATE service worker version " + version)
     return self.skipWaiting()
 })
+self.addEventListener('fetch', () {
+//met en ecoute le services
+})
