3036bd06345945bf439ef8c64db7bfb8de2ac3d8
stagiaire <stagiaire@cpg-453068.cepegra2008.lan>
2019-11-12 14:53:36 +0100

    affichage gare

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 axios.get("https://api.irail.be/stations/?format=json")
     .then ( (response)=> {
-        console.log(response.data.station)
-        let gares = document.querySelector(".gareList")
+        //console.log(response.data.station)
+        let gares = document.querySelector(".garesList")
+        console.log("gares:")
         response.data.station.forEach(element => {
-            gares.innerHTML += `<li>${element}.standarname}</li>`
+            gares.innerHTML += `<li>${element.standardname}</li>`
         })
     })
